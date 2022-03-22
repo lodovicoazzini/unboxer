df68dda647c956b4ea99a6977a52cd5e7d33b13e
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-03-22 22:42:32 +0100

    add ssim and emd emd works only with 1d arrays -> poor performance

--- utils/image_similarity/geometry_based.py
+++ utils/image_similarity/geometry_based.py
@@ -1,6 +1,23 @@
 import numpy as np
 from scipy.stats import wasserstein_distance
+from skimage.metrics import structural_similarity
 
 
 def emd(lhs: np.ndarray, rhs: np.ndarray) -> float:
-    return wasserstein_distance(lhs, rhs)
+    """
+    Compute the earth mover's distance between two matrices
+    :param lhs: The first matrix
+    :param rhs: The second matrix
+    :return: The earth mover's distance between the two matrices
+    """
+    return wasserstein_distance(lhs.flatten(), rhs.flatten())
+
+
+def ssim(lhs: np.ndarray, rhs: np.ndarray) -> float:
+    """
+    Compute the SSIM distance between two matrices
+    :param lhs: The first matrix
+    :param rhs: The second matrix
+    :return: The CW-SSIM distance between the two matrices
+    """
+    return 1 - structural_similarity(lhs, rhs)
