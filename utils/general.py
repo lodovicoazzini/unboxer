f3cc0221da433294e88b2a2cbfb557f58cd703c5
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-03-28 14:53:48 +0200

    add method to save figures

---
+++ utils/general.py
@@ -0,0 +1,17 @@
+import os
+
+import matplotlib.pyplot as plt
+
+
+def beep():
+    os.system('say "beep"')
+
+
+def save_figure(fig: plt.Figure, path: str, dpi: int = 150, transparent=True):
+    # check if the containing directory exists
+    out_dir = '/'.join(path.split('/')[:-1])
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # save the figure
+    fig.savefig(path, dpi=dpi, transparent=transparent)
