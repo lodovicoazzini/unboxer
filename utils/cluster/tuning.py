162dfae374d2d6dad937355cdb826f2abfa2b155
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-03-30 14:39:38 +0200

    simplify tuning method

--- utils/cluster/tuning.py
+++ utils/cluster/tuning.py
@@ -1,27 +1,22 @@
 import numpy as np
 from sklearn.metrics import silhouette_score
 
-from utils.cluster.preprocessing import distance_matrix
-from utils.image_similarity.intensity_based import euclidean_distance
-
 
 def find_optimal_configuration(
         models, data,
-        distance_metric='euclidean',
-        dist_func=euclidean_distance,
-        verbose=False
+        verbose=False,
+        silhouette_metric='euclidean'
 ):
-    # if the distance metric is precomputed -> compute the distance matrix
-    if distance_metric == 'precomputed':
-        dist_data = distance_matrix(data, dist_func=dist_func)
-    else:
-        dist_data = data
     # compute the silhouette scores for all the configurations
     scores = np.array([])
     for model in models:
         clusters = model.fit_predict(data)
-        score = silhouette_score(dist_data, clusters, metric=distance_metric)
-        scores = np.append(scores, score)
+        try:
+            score = silhouette_score(data, clusters, metric=silhouette_metric)
+            scores = np.append(scores, score)
+        except ValueError:
+            # no cluster identified
+            continue
 
     # find the optimal configuration
     optimal = models[np.argmax(scores)]
