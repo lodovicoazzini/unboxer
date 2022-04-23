5fe03d0a5e79adb52c35b596a84ec4d9178bb5f1
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-04-23 15:46:35 +0200

    fix issue with wrong relative path

--- utils/cluster/postprocessing.py
+++ utils/cluster/postprocessing.py
@@ -1,17 +1,18 @@
 import numpy as np
 from clusim.clustering import Clustering
 
+from config_dirs import PREDICTIONS_PATH
 from utils.dataset import get_train_test_data, get_data_masks
 
 
-def get_frac_misses(cluster: list) -> float:
+def get_frac_misses(cluster) -> float:
     """
     Find the fraction of misclassified elements in each cluster.
     """
     # Get the indexes for the misclassified elements
     # Get the indexes of the misclassified elements
     _, (test_data, test_labels) = get_train_test_data(rgb=True)
-    predictions = np.loadtxt('../in/predictions.csv')
+    predictions = np.loadtxt(PREDICTIONS_PATH)
     mask_miss, mask_label = get_data_masks(test_labels, predictions, label=5)
     mask_miss_label = mask_miss[mask_label]
     miss_idxs = np.argwhere(mask_miss_label)
@@ -19,17 +20,17 @@ def get_frac_misses(cluster: list) -> float:
     return len([entry for entry in cluster if entry in miss_idxs]) / len(cluster)
 
 
-def sorted_clusters(clusters: list, sort_by: callable) -> list:
+def sorted_clusters(clusters, metric: callable):
     """
     Sort a list of clusters based on a metric
     """
-    sorting_list = [sort_by(cluster) for cluster in clusters]
+    sorting_list = [metric(cluster) for cluster in clusters]
     zipped = list(zip(clusters, sorting_list))
     sorted_zipped = sorted(zipped, key=lambda entry: entry[1])
     return [cluster for cluster, _ in sorted_zipped]
 
 
-def get_non_unique_membership_list(clusters: list) -> list:
+def get_non_unique_membership_list(clusters) -> list:
     """
     Get the membership list for a list of overlapping clusters.
     In the case of overlap the first occurrence is returned.
