ac091a4cd7dd486bbe4883a1e416fac1d2751c6e
Lodovico Azzini <lodovico.azzini@usi.ch>
2022-05-18 09:32:44 +0200

    add function to compute the average activation over regions of an image

--- utils/images/postprocessing.py
+++ utils/images/postprocessing.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as plt_ticker
+import numpy as np
 
 from config.config_const import IMG_SIZE, GRID_SIZE
 
@@ -25,3 +26,30 @@ def add_grid(ax: plt.Axes) -> plt.Axes:
     ax.grid(b=True, which='major', axis='both', linestyle='-', color='k', zorder=10)
 
     return ax
+
+
+def get_average_activations(
+        image: np.ndarray,
+        threshold: float = None,
+        normalize: bool = False
+):
+    """
+    Average the pixel values of an image
+    :param image: The original image
+    :param threshold: The threshold to ignore lower values
+    :param normalize: Whether to normalize the values in [0, 1)
+    :return: The resized image
+    """
+    processed = image
+    if normalize or threshold is not None:
+        processed = image * (1 - np.nanmin(image)) / (np.nanmax(image) - np.nanmin(image))
+    if threshold is not None:
+        processed = np.ma.masked_less_equal(processed, threshold).filled(np.nan)
+        processed = processed * (1 - np.nanmin(processed)) / (np.nanmax(processed) - np.nanmin(processed))
+    averaged = np.nanmean(
+        np.nanmean(
+            processed.reshape((GRID_SIZE, processed.shape[0] // GRID_SIZE, GRID_SIZE, -1)),
+            axis=3),
+        axis=1
+    )
+    return averaged, np.nanargmax(averaged)
