fix wrong calculation for the silhouette score in case of original mode

utils/clusters/Approach.py
@@ -144,15 +144,17 @@ class OriginalMode(Approach):
         # Compute the similarity matrix for the contributions
         similarity_matrix = compute_comparison_matrix(
             list(contributions),
-            metric=self.multiprocessing_metric,
+            metric=IMAGES_SIMILARITY_METRIC,
             show_progress_bar=True,
-            multi_process=True
+            multi_process=False
         )
         # Cluster the contributions using the similarity matrix
         clusters = self.get_clustering_technique()(affinity='precomputed').fit_predict(similarity_matrix)
         # Compute the silhouette for the clusters
         try:
-            score = silhouette_score(1 - similarity_matrix, clusters, metric='precomputed')
+            distance_matrix = 1 - similarity_matrix
+            np.fill_diagonal(distance_matrix, 0)
+            score = silhouette_score(distance_matrix, clusters, metric='precomputed')
         except ValueError:
             score = np.nan
 